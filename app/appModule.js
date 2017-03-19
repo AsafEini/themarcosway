1d6333ba34764236d4b3bb329d610036bde1ab3f
tankian tankian <tankian tankian>
2017-03-19 12:59:04 +0200

    marcooo

---
+++ app/appModule.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var main_1 = require("./main");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(main_1.AppModule);
+//# sourceMappingURL=appModule.js.map
